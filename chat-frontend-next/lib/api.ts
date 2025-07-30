62e1722bec51be528ab50670807c70fd48f2377d
wilfierd <vexbravor@gmail.com>
2025-07-30 23:58:54 +0700

    update login form

--- chat-frontend-next/lib/api.ts
+++ chat-frontend-next/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // backend URL
+  baseURL: 'http://localhost:8080/api', // backend URL
 });
 
 api.interceptors.request.use(config => {
