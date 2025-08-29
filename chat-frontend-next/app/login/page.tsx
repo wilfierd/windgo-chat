e177060b6d066d3015c069ded36a167b6947b6aa
Wilfierd-linux <vexbravor@gmail.com>
2025-08-29 23:57:17 +0700

    update move fe to another repo

--- chat-frontend-next/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from '@/components/LoginForm';
-
-export default function LoginPage() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-gray-50">
-      <LoginForm />
-    </div>
-  );
-} 
